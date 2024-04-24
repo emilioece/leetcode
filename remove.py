25376a22e543ed73599e4e983a6a4d6ee6862796
emilioece <emailemilioee@gmail.com>
2024-04-24 14:42:53 -0700

    27: remove element

---
+++ remove.py
@@ -0,0 +1,12 @@
+def remove(nums:list, val:int):
+    nums.sort()
+    k = len(nums)
+    while val in nums:
+        nums.remove(val)
+        k-=1
+    print(nums, k)
+
+if __name__ == '__main__':
+    nums = [0,1,2,2,3,0,4,2]
+    val = 2
+    remove(nums, val)
